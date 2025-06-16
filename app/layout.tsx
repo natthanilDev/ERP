c4e4dd1eb61a336b9248cb4e457eaf59b9503547
Natthanil Wannasopa <J2505001@JIEI.local>
2025-06-16 10:55:46 +0700

    coding

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import Navbar from "./component/navbar/navabar";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-
+        <Navbar />
         {children}
 
       </body>
